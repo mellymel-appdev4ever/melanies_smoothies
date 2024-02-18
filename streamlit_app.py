b29b9f8e8c5b982b8d1a766aaf2d03d53912a6fc
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2024-02-18 13:57:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ if ingredients_list:
 
 	for fruit_chosen in ingredients_list:
 		ingredients_string += fruit_chosen + ' '
+		search_on=fv_df.loc[fv_df['FRUIT_NAME'] == fruit_name, 'SEARCH_ON'].iloc[0]
+		st.write('The search value for ', FRUIT_NAME,' is ', SEARCH_ON, '.')
 		st.subheader(fruit_chosen + ' Nutrition Information')
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
@@ -45,6 +47,9 @@ if ingredients_list:
 	if time_to_insert:
 		session.sql(my_insert_stmt).collect()
 		st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+
+
 
 
 
