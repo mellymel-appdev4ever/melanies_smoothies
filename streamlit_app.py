b2b9e571e029e354ea3580eb56285201c312915f
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2024-02-18 14:31:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+h_# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -32,7 +32,7 @@ if ingredients_list:
 	for fruit_chosen in ingredients_list:
 		ingredients_string += fruit_chosen + ' '
 		search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-		st.write('The search value for ', fruit_chosen,' is ', SEARCH_ON, '.')
+		st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 		st.subheader(fruit_chosen + ' Nutrition Information')
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
