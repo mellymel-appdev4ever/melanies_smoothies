28a2c63657d88d3d15eb1dfac1bc1e7dd42dd8cd
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2024-06-16 08:27:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ session = cnx.session()
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response).json())
-fv_df = st.dataframe(data=fruityvice_response).json(), use_container_width=True)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
